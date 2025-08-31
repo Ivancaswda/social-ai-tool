309fa485aaba4d062166e924f90c042cca6350ac
aivanius <aivanius@yandex.ru>
2025-08-31 20:56:27 +0700

    sgagsgas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true, // отключает ESLint при сборке
+    },
+    typescript: {
+        ignoreBuildErrors: true, // отключает ошибки TypeScript при сборке
+    },
     images: {
         domains: ['ik.imagekit.io', 'i.ytimg.com']
     }
